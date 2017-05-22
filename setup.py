72b41476916a781d055382263a2d4446738497a2
Kumar Pratik <kumar.pratik73@yahoo.com>
2017-05-22 05:10:40 +0000

    adding README.md

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='myproject',
+    author='Kumar Pratik',
+    description='Nginx server for Flask',
+    author_email='kumar.pratik73@yahoo.com',
+    packages=['myproject'],
+    include_package_data=True,
+    install_requires=[
+        'flask','flask-sqlalchemy',
+    ],
+    scripts=['bin/myproject-shell']
+)
