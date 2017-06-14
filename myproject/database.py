d785b076f1a6a5be380613d9c816c699e5464ea2
Kumar Pratik <kumar.pratik73@yahoo.com>
2017-06-14 10:48:19 +0530

    Modified Flush rates and variable

--- myproject/database.py
+++ myproject/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine('postgresql:///xyzdb', convert_unicode=True)
 metadata = MetaData(bind=engine)
 db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
+                                         autoflush=True,
                                          bind=engine))
 def init_db():
     metadata.create_all(bind=engine)
