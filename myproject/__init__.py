32ff3d95dfd461f089ac8bcc59a81caa69f97a26
Kumar Pratik <kumar.pratik73@yahoo.com>
2017-06-20 07:51:47 +0000

    Error

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,13 +1,10 @@
 #Main application file. The routes are imported from views, data and record accordingly. 
-
 from flask import Flask
 app = Flask(__name__, static_folder='/static')
-
 #Route methods are imported below
 import myproject.views
 import myproject.record
 import myproject.data
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug = True)
