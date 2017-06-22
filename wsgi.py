fd0d839c4b462855622e99557bea707b2c815dac
Kumar Pratik <kumar.pratik73@yahoo.com>
2017-06-22 11:13:56 +0530

    Nothing

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from myproject import app
-import logging 
-logging.basicConfig(filename='./error.log', level=logging.DEBUG)
 
 if __name__ == "__main__":
+	import logging 
+	logging.basicConfig(filename='./error.log', level=logging.DEBUG)
     app.run()
