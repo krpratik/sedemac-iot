45cabe00a864201e5d2e986ffb4f8e8a8905f136
Kumar Pratik <kumar.pratik73@yahoo.com>
2017-06-22 12:38:57 +0530

    trying

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from myproject import app
-
+import logging
+logHandler = logging.FileHandler('./sedemac_iot_error.log')
+logHandler.setLevel(logging.DEBUG)
+app.logger.addHandler(logHandler)
+app.logger.setLevel(logging.DEBUG)
 if __name__ == "__main__":
     app.run()
